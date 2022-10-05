0b99b74b39e2f5adfb696435b1d3afea0562d08f
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-10-05 19:58:26 +0200

    update

--- OOP1/src/Task2/Main.java
+++ OOP1/src/Task2/Main.java
@@ -1,9 +1,46 @@
 package Task2;
 
+import java.util.ArrayList;
+
 public class Main
 {
     public static void main(String[] args)
     {
         // TODO in class building, check/make 2.d
+
+        // 2.g instantiate at least three different rooms
+        Room room1 = new Room(1,2,3);
+        Room room2 = new Room(2,4,2);
+        Room room3 = new Room(1,3,1);
+
+        // 2.h Add the three rooms to the "Rooms"
+        ArrayList<Room> rooms = new ArrayList<>();
+        rooms.add(room1);
+        rooms.add(room2);
+        rooms.add(room3);
+
+
+
+        // 2.I Instantiate a new building.
+
+        Building building1 = new Building(rooms,5,2,false);
+
+        System.out.println(building1.toString());
+
+
+
+
     }
+
+
+    // 2.j  create a static method in Main, countLampsInBuilding
+    public static void countLampsInBuilding(Building building)
+    {
+
+        System.out.println(building.getRooms().get(1).getNumberOfLamps());
+
+
+
+    }
+
 }
