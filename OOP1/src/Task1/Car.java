e4ff6d630ccd048fba100a00e405917019387843
cph-oo221 <113044607+cph-oo221@users.noreply.github.com>
2022-10-05 15:21:33 +0200

    update

---
+++ OOP1/src/Task1/Car.java
@@ -0,0 +1,10 @@
+package Task1;
+
+public class Car
+{
+    private x make;
+    private x model;
+    private x year;
+    private x bodyStyle;
+    private x Driver;
+}
---
+++ OOP1/src/Task1/Driver.java
@@ -0,0 +1,27 @@
+package Task1;
+
+public class Driver
+{
+
+    // 2 private fields:
+    private String name;
+    private int age;
+
+    // Constructor
+    public Driver(String name, int age)
+    {
+        this.name = name;
+        this.age = age;
+    }
+
+    // getter
+    public String getName()
+    {
+        return name;
+    }
+
+    public int getAge()
+    {
+        return age;
+    }
+}
